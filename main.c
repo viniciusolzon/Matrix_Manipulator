6fc9c5ea5d941a3cd631683c214df0a230eb3f4b
Vinicius Freitas <vinicius_freitas03@hotmail.com>
2022-06-04 16:02:29 -0300

    zeros_matrix and full_matrix ok

--- main.c
+++ main.c
@@ -6,10 +6,14 @@
 int main(){
 
     int data[16] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
-    
-    Matrix matriz = create_matrix(data, 4, 4);
+    Matrix matriz1 = create_matrix(data, 4, 4);
+    print_matrix(matriz1);
 
-    print_matrix(matriz);
+    Matrix matriz2 = zeros_matrix(3, 4);
+    print_matrix(matriz2);
+
+    Matrix matriz3 = full_matrix(3, 3, 1);
+    print_matrix(matriz3);
 
     return 0;
 }
